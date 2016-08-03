a143d2095d69deff32c5f350f5a21c2f89c62bfb
Asmund Berge <herrasbe@gmail.com>
2016-08-03 20:41:05 +0200

    Simplified replacement logic for missing matches

--- rename.py
+++ rename.py
@@ -7,50 +7,49 @@ argParser.add_argument("filename", help = "file to do replacements on")
 
 args = argParser.parse_args()
 
-replacements = {}
-newReplacements = {}
 filename = args.filename
 replacementFilename = args.replacementsfile
 pattern = args.pattern
+replacements = {}
 ignorewords = []
+replaceCount = 0
+newReplacements = 0
+
+def sub(this, that, line):
+    global replaceCount
+    repLine = line
+    for a, b in [(this, that), (this.upper(), that.upper())]:
+        repLine, n = re.subn(r"\b{}\b".format(a), b, repLine)    
+        replaceCount += n
+    return repLine
 
 with open(replacementFilename, 'r') as r:
     for line in r:
         words = line.split() 
         if len(words) == 2:
             replacements[words[0]] = words[1]
-            replacements[words[0].upper()] = words[1].upper()
         else:
             raise Exception("Syntax error in replacement file")
 
-print("Loaded {} replacements from {}".format(len(replacements) // 2, replacementFilename))
+print("Loaded {} replacements from {}".format(len(replacements), replacementFilename))
                 
 with open(filename) as infile, open("OUT_" + filename, 'w') as outfile:
-    replaceCount = 0
     for line in infile:
         for key in replacements:
-            line, n = re.subn(r"\b{}\b".format(key), replacements[key], line)    
-            replaceCount += n
+            line = sub(key, replacements[key], line)
         if pattern:
-            for match in sorted(set(re.findall(pattern, line))):
-                match = match.lower()
-                if match in ignorewords:
-                    continue
+            for match in [ m.lower() for m in sorted(set(re.findall(pattern, line))) \
+                                                        if m.lower() not in ignorewords]:
                 rep = input("Enter a replacement for '{}': ".format(match)).lower()
                 if not rep:
                     ignorewords.append(match)
                     continue
-                newReplacements[match] = rep
-                newReplacements[match.upper()] = rep.upper()
-                replacements[match]    = rep
-                replacements[match.upper()]    = rep.upper()
-            for key in newReplacements:
-                line, n = re.subn(r"\b{}\b".format(key), newReplacements[key], line)    
-                replaceCount += n
+                replacements[match] = rep
+                line = sub(match, rep, line)
         outfile.write(line)
     print("Performed {} replacements in {}.".format(replaceCount, filename))
 
-if len(newReplacements):
-    print("Got {} new replacements. rewriting {}.".format(len(newReplacements) // 2, replacementFilename))
+if newReplacements:
+    print("Got {} new replacements. rewriting {}.".format(newReplacements, replacementFilename))
     with open(replacementFilename, 'w') as r:
-        r.writelines(["{} {}\n".format(k, replacements[k]) for k in replacements if k.islower()])
+        r.writelines(["{} {}\n".format(k, replacements[k]) for k in replacements])
